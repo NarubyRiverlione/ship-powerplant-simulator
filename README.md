c2327d57e929cce91a05aa85cf13bdb0dca71cb9
Naruby Rivelione <naruby.riverlione@gmail.com>
2021-02-15 13:42:32 +0100

    safety valve computed value

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ simulation of a ship engine power plant
 
 | Statements                  | Branches                | Functions                 | Lines                |
 | --------------------------- | ----------------------- | ------------------------- | -------------------- |
-| ![Statements](https://img.shields.io/badge/Coverage-100%25-brightgreen.svg) | ![Branches](https://img.shields.io/badge/Coverage-99.51%25-brightgreen.svg) | ![Functions](https://img.shields.io/badge/Coverage-99.29%25-brightgreen.svg) | ![Lines](https://img.shields.io/badge/Coverage-100%25-brightgreen.svg)    |
+| ![Statements](https://img.shields.io/badge/Coverage-100%25-brightgreen.svg) | ![Branches](https://img.shields.io/badge/Coverage-99.51%25-brightgreen.svg) | ![Functions](https://img.shields.io/badge/Coverage-99.3%25-brightgreen.svg) | ![Lines](https://img.shields.io/badge/Coverage-100%25-brightgreen.svg)    |
 
 ## Power systems
 ### Switchboard
--- src/Components/Compressor.ts
+++ src/Components/Compressor.ts
@@ -9,26 +9,24 @@ export default class Compressor extends Appliance {
   Output: number
   RatedFor: number
   OutletValve: Valve
-  SafetyOpen: boolean
   HasReceiver: boolean
 
   constructor(name: string, bus: PowerBus, rate: number) {
     super(name, bus)
     this.Output = 0.0
     this.RatedFor = rate
-    this.SafetyOpen = false
     this.HasReceiver = false
     this.OutletValve = new Valve(name + ' - outlet valve', this)
-    makeObservable(this, { Output: observable, Content: computed })
+    makeObservable(this, { HasReceiver: observable, SafetyOpen: computed, Output: observable, Content: computed })
   }
 
   get Content() { return this.Output }
 
+  // open safety with running without a receiver
+  get SafetyOpen() { return this.isRunning && !this.HasReceiver }
+
   Thick() {
     super.Thick()
-    // open safety with running without a receiver
-    this.SafetyOpen = this.isRunning && !this.HasReceiver
-
     // only output when running with closed safety
     this.Output = !this.SafetyOpen && this.isRunning ? this.RatedFor : 0.0
     this.OutletValve.Source = this
